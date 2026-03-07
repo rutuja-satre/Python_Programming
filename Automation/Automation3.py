4e368e9ba2d747d9980eca096dc2e569800fa78d
Rutuja Satre <rutusatre355@gmail.com>
2026-03-07 19:26:48 +0530

    Create Automation3.py

---
+++ Automation/Automation3.py
@@ -0,0 +1,18 @@
+import schedule
+import time
+import datetime
+
+def MySchedule():
+    print("Inside my schedule function at : ",datetime.datetime.now())
+
+def main():
+    print("Inside automation script")
+    print("Current time is : ",datetime.datetime.now()) 
+
+    schedule.every(20).seconds.do(MySchedule) 
+ 
+    print("Application is in waiting state : ")
+    time.sleep(50)
+
+if __name__=="__main__":
+    main()
